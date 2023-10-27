add7479b6e47996855196b7e8d180de5e1ef8764
Ahnaf <ahnafrahman71@gmail.com>
2023-10-27 17:46:34 +0600

    Firebase Chat

---
+++ firebase_chat/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_chat/authenticate/authenticate.dart';
+import 'package:firebase_chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Authenticate(),
+    );
+  }
+}
